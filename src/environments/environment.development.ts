00e02666cc068bc4840b4b30f8ba66e6055509c0
Flavia Hotts <flaviahotts@flavias-air.home>
2023-05-15 00:33:19 +0100

    initial setup

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyAQX8g_SvvDScy1GHwIMZSXJ6y3etGrDlA",
+        authDomain: "todo-list-86d54.firebaseapp.com",
+        projectId: "todo-list-86d54",
+        storageBucket: "todo-list-86d54.appspot.com",
+        messagingSenderId: "148527326428",
+        appId: "1:148527326428:web:d686950bb0ae88ff0bc019"
+    }
+};
