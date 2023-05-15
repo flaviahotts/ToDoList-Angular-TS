63cf29415d617bb8aff20f7b95961b9ffc5082db
Flavia Hotts <flaviahotts@flavias-air.home>
2023-05-15 11:26:07 +0100

    on-click features and some styles

--- src/app/shared/todo.service.ts
+++ src/app/shared/todo.service.ts
@@ -17,4 +17,15 @@ export class TodoService {
       isDone : false
     })
   }
+
+
+  updateTodoStatus(id: string, newStatus:boolean){
+  this.firestoreCollection.doc(id).update({isDone:newStatus})
+  }
+
+  deleteTodo(id:string){
+    this.firestoreCollection.doc(id).delete()
+  }
+
 }
+
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -9,14 +9,25 @@ import { TodoService } from '../shared/todo.service';
 })
 export class TodoComponent implements OnInit{
 
+  todos: any[] = [];
+
   constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
+    this.todoService.firestoreCollection.valueChanges({idField: 'id'}).subscribe(item=> { 
+      this.todos = item.sort((a:any, b:any)=>
+      {return a.isDone -b.isDone}); 
+    })
   }
 
   onClick(titleInput: HTMLInputElement){
     if(titleInput.value)
-  this.todoService.addTodo(titleInput.value);
-  titleInput.value = " ";
+      this.todoService.addTodo(titleInput.value);
+      titleInput.value = "";
+  }
+
+  onStatusChange(id: string, newStatus: boolean){
+    this.todoService.updateTodoStatus(id, newStatus);
   }
 }
+
